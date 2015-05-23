6ff1bec3f3aa9c2727dfb24464d21f787e35dfc3
Stefania Sicurelli <stefsicart@gmail.com>
2015-05-23 14:20:14 -0400

    add form to events new

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, presence: true
-  validates :location, presence: true
+  # validates :name, presence: true
+  # validates :location, presence: true
 end
