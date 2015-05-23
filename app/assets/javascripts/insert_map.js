87ca66b6e4b3f9dc25ac43b92a14c4db4f953d18
stefsic <verdi5588>
2015-05-23 11:34:12 -0400

    add marker to map

--- app/assets/javascripts/insert_map.js
+++ app/assets/javascripts/insert_map.js
@@ -1,19 +1,28 @@
 $(document).on("ready", function(){
   // debugger;
-  var latitude = -34.397;
-  var longitude = 150.644;
+  // var latitude = -34.397;
+  // var longitude = 150.644;
 
-  function insertMap(latitude, longitude) {
+  function insertMap() {
+
+    var myLatlng = new google.maps.LatLng(-34.397, 150.644);
 
     var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
-      center: { lat: -34.397, lng: 150.644 },
+      center: myLatlng,
       zoom: 8,
       mapTypeId: google.maps.MapTypeId.TERRAIN
     };
 
     var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Hello World!'
+      });
   }
 
-  insertMap(latitude, longitude);
+  insertMap();
+  // marker();
 });
