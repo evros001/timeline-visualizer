433118e14f624656f7f8e721805dbfae778213fc
stefsic <verdi5588>
2015-05-23 16:08:16 -0400

    user can add events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,16 +4,27 @@ class EventsController < ApplicationController
      @event = Event.new
     #  @user = User.find_by(params[:id])
    end
+   #
+  #  def create
+  #    @event = Event.new(safe_params)
+  #    @event.user_id = params[:user_id]
+  #    @event.save
+  #    redirect_to user_path(params[:user_id])
+  #  end
 
    def create
-     @event = Event.create(safe_params)
-    #  binding.pry
-    #  @user = User.find_by(params[:id])
+      @event = Event.create(safe_params)
+      @user = User.find(params[:user_id])
+      @event.user_id = @user.id
+      @user.events << @event
      redirect_to user_path(params[:user_id])
    end
 
   private
    def safe_params
-     params.require(:event).permit(:name, :latitude, :longitude, :user_id)
+     params.require(:event).permit(:name, :latitude, :longitude)
    end
 end
+
+
+#params[:user_id] = 2
