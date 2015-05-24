eb830231589d9aeb0fbb2a47be20ba8901c7cb59
stefsic <verdi5588>
2015-05-24 15:01:56 -0400

    add geocoer gem and change events to locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523194721) do
+ActiveRecord::Schema.define(version: 20150524183907) do
 
-  create_table "events", force: :cascade do |t|
+  create_table "locations", force: :cascade do |t|
     t.string   "name"
-    t.string   "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "events", ["user_id"], name: "index_events_on_user_id"
+  add_index "locations", ["user_id"], name: "index_locations_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
