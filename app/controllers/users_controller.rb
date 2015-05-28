b3a85483088fbc149c3621abf430ef1ca9b3a15e
evros001 <evanrosse@gmail.com>
2015-05-28 17:28:46 -0400

    seeded stories for presentation seed data not persisted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :markers]
+  before_action :verify_user, only: [:edit, :destroy]
 
   def index
     @users = User.all
@@ -14,16 +16,31 @@ class UsersController < ApplicationController
       @markers = @user.markers      
   end
 
+  def markers
+    # @user = User.find(params[:id])
+    @markers = @user.markers
+    respond_to do |format|
+        format.json {render "../views/stories/markers"}
+    end
+  end
+
   def create
     @user = User.create(safe_params)
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def safe_params
     params.require(:user).permit(:name, :email, :story_id, :location_id)
+  end
+
+  def verify_user
+
+    if !user_signed_in? || current_user.id.to_s != params[:user_id]
+      redirect_to root_path
+    end
   end
 end
