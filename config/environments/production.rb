b93e4a22f568d9a38ca37d92f1d2df5ffd9fc7e4
stefsic <verdi5588>
2015-05-29 12:08:34 -0400

    update application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   GA.tracker = Rails.application.secrets.google_analytics_code
   # Settings specified here will take precedence over those in config/application.rb.
 
